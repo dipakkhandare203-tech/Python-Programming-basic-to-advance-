982a1f5f60fe86284331937cb2d7615ac43b5780
dipakkhandare203-tech <dipakkhandare203@gmail.com>
2025-08-22 17:01:26 +0530

    Simple Input Program

--- A program to take input.py
+++ A program to take input.py
@@ -13,8 +13,26 @@ print("Enter your Average Marks")
 avgMarks=float(input())
 
 #Print Entered Details
-print("Re-entered your details")
+print("Confirm Your Details")
 print("Name of student:-",nm)
 print("Age:-",age)
 print("RollNo:-",roll)
 print("AvgMarks",avgMarks)
+
+#Output
+'''
+PS D:\python programmming> & "C:/Program Files/Python313/python.exe" "d:/python programmming/A program to take input.py"
+Enter your name
+Dipak Khandare
+Enter your Age
+20
+Enter your RollNo
+1331
+Enter your Average Marks
+90.67
+Re-entered your details
+Name of student:- Dipak Khandare
+Age:- 20
+RollNo:- 1331
+AvgMarks 90.67
+PS D:\python programmming> '''
