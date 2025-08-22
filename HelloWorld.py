7f0851bc599b154a3a4bfd77b9ee9cbb7352b666
dipakkhandare203-tech <dipakkhandare203@gmail.com>
2025-08-22 16:12:08 +0530

    Hello World Program

---
+++ HelloWorld.py
@@ -0,0 +1,12 @@
+print("hello world")
+print("you will get this in new line")
+print("you will get this",end="")
+print("in same line")
+
+#output
+''' 
+PS D:\python programmming> & "C:/Program Files/Python313/python.exe" "d:/python programmming/HelloWorld.py"
+hello world
+you will get this in new line
+you will get thisin same line
+PS D:\python programmming> '''
