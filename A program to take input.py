db2808c75c1eabe56c809539efb3c952e777ba7d
dipakkhandare203-tech <dipakkhandare203@gmail.com>
2025-08-22 16:56:37 +0530

    Simple input Program

---
+++ A program to take input.py
@@ -0,0 +1,20 @@
+#Take a input as Name
+print("Enter your name")
+nm=input()
+
+#Take another input as Age, RollNo and AverageMarks
+print("Enter your Age")
+age=int(input())
+
+print("Enter your RollNo")
+roll=int(input())
+
+print("Enter your Average Marks")
+avgMarks=float(input())
+
+#Print Entered Details
+print("Re-entered your details")
+print("Name of student:-",nm)
+print("Age:-",age)
+print("RollNo:-",roll)
+print("AvgMarks",avgMarks)
