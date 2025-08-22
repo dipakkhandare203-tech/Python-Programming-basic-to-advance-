3a072f31050400587aaff152cf3cc04505ad3dde
dipakkhandare203-tech <dipakkhandare203@gmail.com>
2025-08-22 16:34:13 +0530

    Variable Declaration

---
+++ Variable Declaration.py
@@ -0,0 +1,38 @@
+#String Variable
+name = "Dipak"
+
+#Integer Variable 
+age = 21
+
+#Float Variable 
+pi = 3.14
+
+#Boolean Variable
+is_student = True
+
+#Multiple Variable
+x,y,z = 10,20,30 
+a=b=c ="Python"
+
+print("Name",name)
+print("Age",age)
+print("Pi",pi)
+print("Student",is_student)
+
+print("X",x)
+print("X,Y,Z",x,y,z)
+
+print("a",a)
+print("b",b)
+print("c",c)
+
+#Output
+'''
+PS D:\python programmming> & "C:/Program Files/Python313/python.exe" "d:/python programmming/Variable Declaration.py"
+Name Dipak
+Age 21
+Pi 3.14
+Student True
+PS D:\python programmming> '''
+
+
